f6c26029540cb9841fc2cfa330f48c9d64d7e488
Brice789 <107217757+Brice789@users.noreply.github.com>
2023-01-08 17:37:43 +0100

    Add files via upload

---
+++ QrCODE/QrCODE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QrCODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] StringToChar(Lewis);
+            Console.
+        }
+    }
+}
---
+++ QrCODE/QrCODE/QrCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QrCODE
+{
+    public class QrCode
+    {
+        public QrCode()
+        {
+            char[] StringToChar(string mot)
+            {
+
+                char[] lettre = mot.ToCharArray(0, mot.Length );
+                Console.WriteLine( lettre );
+
+            }
+        }
+    }
+}
